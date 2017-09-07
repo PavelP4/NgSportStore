4e125cdda6efda116743f8ab8bc21ba9a407921b
Pavel <reginuser@gmail.com>
2017-09-07 16:05:17 +0300

    Chapter 10-197

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,8 +8,16 @@ import { CartDetailComponent } from "./store/cartDetail.component";
 import { RouterModule } from "@angular/router";
 import { StoreFirstGuard } from "./storeFirst.guard";
 
+import { AdminModule } from "./admin/admin.module";
+import { AuthComponent } from "./admin/auth.component";
+import { AdminComponent } from "./admin/admin.component";
+import { AuthGuard } from "./admin/auth.guard";
+import { ProductTableComponent } from "./admin/productTable.component";
+import { ProductEditorComponent } from "./admin/productEditor.component";
+import { OrderTableComponent } from "./admin/orderTable.component";
+
 @NgModule({
-    imports: [BrowserModule, StoreModule,
+    imports: [BrowserModule, StoreModule, AdminModule,
         RouterModule.forRoot([
             { 
                 path: "store", component: StoreComponent,
@@ -22,7 +30,24 @@ import { StoreFirstGuard } from "./storeFirst.guard";
                 canActivate: [StoreFirstGuard] },
             {
                 path: "admin",
-                loadChildren: "app/admin/admin.module#AdminModule",
+                children: [
+                    { path: "auth", component: AuthComponent },
+                    {
+                        path: "main", component: AdminComponent,
+                        canActivate: [AuthGuard],
+                        children: [
+                            { path: "products/:mode/:id",
+                                component: ProductEditorComponent },
+                            { path: "products/:mode",
+                                component: ProductEditorComponent },
+                            { path: "products", component: ProductTableComponent },
+                            { path: "orders", component: OrderTableComponent },
+                            { path: "**", redirectTo: "products" }
+                        ]
+                    },
+                    { path: "**", redirectTo: "auth" }
+                ],
+                //loadChildren: "app/admin/admin.module#AdminModule",                
                 canActivate: [StoreFirstGuard]
             },
             { path: "**", redirectTo: "/store" }
